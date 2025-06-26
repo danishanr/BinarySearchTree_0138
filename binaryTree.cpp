60335d2ef34eef6c37b08a7619064acde51f2276
danishanr <danishaanr@gmail.com>
2025-06-26 15:01:44 +0700

    Membuat class Node

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
 using namespace std;
 
+class Node 
+{
+public :
+    int info;
+    Node *leftchild;
+    Node *rightchild;
+
+    // Constructor for node class
+    Node()
+    {
+        leftchild = nullptr; // Intialiaze left child to null
+        rightchild = nullptr; // Intialize right child to null
+    }
+};
