e91d0fb9db68279012901daa0c0ba6d3b59da271
danishanr <danishaanr@gmail.com>
2025-06-26 15:16:45 +0700

    Step 3: Make the left and right child of the new node point to NULL

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -51,6 +51,11 @@ public :
         Node *newNode = new Node();
         // Step 2: Assign value to the datafield of the new node
         newNode->info = x;
+        // Step 3: Make the left and right child of the new node point to NULL
+        newNode->leftchild = nullptr;
+        newNode->rightchild = nullptr;
+
+        
     }
     
 };
