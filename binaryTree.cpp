37875f3e62b3bd3389d9f2e2debac1f65330e726
danishanr <danishaanr@gmail.com>
2025-06-26 15:19:22 +0700

    Step 5: If parent is NULL (Tree is empty)

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -62,6 +62,12 @@ public :
         Node *currentNode = nullptr;
         search(x, parent, currentNode);
 
+        // Step 5: If parent is NULL (Tree is empty)
+        if (parent == nullptr)
+        {
+            
+        }
+
 
 
     }
